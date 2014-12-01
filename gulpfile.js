fc0ed6a040f38e8cfbb426b2b315ff4e1562d2cf
Yuji Sugiura <yuji.sugiura@dena.com>
2014-12-01 11:39:54 +0900

    const test dir

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,16 @@ var gulp    = require('gulp');
 var espower = require('gulp-espower');
 var mocha   = require('gulp-mocha');
 
+var TEST_DIR = './test',
+    ESPD_DIR = './test/espowered';
+
 gulp.task('test:make', function() {
-    gulp.src(['test/*.js'])
+    gulp.src([TEST_DIR + '/*.js'])
         .pipe(espower())
-        .pipe(gulp.dest('test/espowered'));
+        .pipe(gulp.dest(ESPD_DIR));
 });
 
 gulp.task('test:exec', function() {
-    gulp.src(['test/espowered/*.js'])
+    gulp.src([ESPD_DIR + '/*.js'])
         .pipe(mocha());
 });
