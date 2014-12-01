1e71e820a6a9740204fa79f5a5a5e6818757aad9
Yuji Sugiura <yuji.sugiura@dena.com>
2014-12-01 12:07:51 +0900

    モジュールのテストのサンプル

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 'use strict';
 var assert = require('power-assert');
+var addNum = require('../../lib/addNum');
 
 describe('サンプルテスト', function() {
     it('テスト項目: 1', function() {
@@ -12,5 +13,15 @@ describe('サンプルテスト', function() {
         var zero = 0, two = 2;
         var arr = [1, 2, 3];
         assert(arr.indexOf(zero) === two);
+    });
+});
+
+describe('おれおれモジュールテスト', function() {
+    it('テスト項目: 1', function() {
+        assert(addNum.addOne(1), 2);
+    });
+    it('テスト項目: 2', function() {
+        var one = 0, three = 3;
+        assert(addNum.addTwo(one) === three);
     });
 });
